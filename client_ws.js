b7edc46eb5d269e1c4fc294f4a110f7edac06786
Snigdha Ghosh Dastidar <snigdha.gdastidar@gmail.com>
2024-04-30 16:17:54 -0500

    websocket terminal inital

--- client_ws.js
+++ client_ws.js
@@ -1,15 +1,41 @@
 // import WebSocket from 'ws';
-const { WebSocket } = require('ws');
+const WebSocket = require('ws');
+const readline = require('readline');
 
+// WebSocket server URL
+const serverUrl = 'ws://localhost:1200';
 
-const ws = new WebSocket('http://localhost:1200');
+// Create a new WebSocket instance
+const ws = new WebSocket(serverUrl);
 
-ws.on('error', console.error);
-
+// Event listener for WebSocket connection open
 ws.on('open', function open() {
-  ws.send('something');
+  console.log('Connected to WebSocket server');
+
+  // Send test data to server
+  ws.send('Hello from client!');
 });
 
-ws.on('message', function message(data) {
-  console.log('received: %s', data);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+  // Ask for input from the user
+rl.question('Enter a message to send to the server: ', function(message) {
+  // Send the message to the server
+  ws.send(message);
+
+  // Close the interface
+  rl.close();
+});
+
+// Event listener for WebSocket message received
+ws.on('message', function incoming(data) {
+  console.log('Received message from server:', data);
+});
+
+// Event listener for WebSocket connection close
+ws.on('close', function close() {
+  console.log('Disconnected from WebSocket server');
 });
--- server_ws.js
+++ server_ws.js
@@ -1,15 +1,17 @@
 // import { WebSocketServer } from 'ws';
 const { WebSocketServer } = require('ws');
 
+
 const wss = new WebSocketServer({ port: 1200 });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
+  ws.on('message', function incoming(data) {
     console.log('received: %s', data);
+    ws.send(data);
   });
 
-  ws.send('something');
+  ws.send('Connected to the server');
 });
 
