1ca04c1c5b4288ed0253ba5b2d727da1025b622d
Snigdha Ghosh Dastidar <snigdha.gdastidar@gmail.com>
2024-04-30 19:35:20 -0500

    websocket multiple client to one server

--- server_ws.js
+++ server_ws.js
@@ -1,13 +1,20 @@
 const { WebSocketServer } = require('ws');
 
 const wss = new WebSocketServer({ port: 1200 });
+const clients = []; // Array to store connected WebSocket instances
+
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
+  clients.push(ws);
 
   ws.on('message', function incoming(data) {
     console.log('received: %s', data);
-    ws.send(data);
+    // Broadcast the received message to all connected clients
+    // clients.forEach(client => {
+    //   client.send(data);
+    // });
+    ws.send(data.toString());
   });
 
   ws.send('Connected to the server');
