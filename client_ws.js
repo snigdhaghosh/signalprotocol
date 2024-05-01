46ec50a091c93f52dd290e22373f9c3d07eb2d92
Snigdha Ghosh Dastidar <snigdha.gdastidar@gmail.com>
2024-05-01 12:24:51 -0500

    keep track of client

--- client_ws.js
+++ client_ws.js
@@ -1,32 +1,64 @@
-const WebSocket = require('ws');
-const readline = require('readline');
+const ws = new WebSocket('ws://localhost:1200');
 
-const serverUrl = 'ws://localhost:1200';
-const ws = new WebSocket(serverUrl);
+ws.onopen = function(event) {
+    console.log('Connected to WebSocket server');
+};
 
-ws.on('open', function open() {
-  console.log('Connected to WebSocket server');
-});
+ws.onmessage = function(event) {
+    console.log('Received message from server:', event.data);
+    // Display received message in the chat window
+    var chatMessages = document.getElementById('chatMessages');
+    chatMessages.innerHTML += '<p>' + event.data + '</p>';
+};
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+ws.onerror = function(error) {
+    console.error('WebSocket error:', error);
+};
 
-// Function to continuously accept user input and send it to the server
-function sendMessages() {
-  rl.question('Enter a message to send to the server: ', function(message) {
+ws.onclose = function() {
+    console.log('Disconnected from WebSocket server');
+};
+
+// Function to send a message to the server
+function sendMessage() {
+    var messageInput = document.getElementById('messageInput');
+    var message = messageInput.value;
     ws.send(message);
-    sendMessages(); // Recursively call sendMessages to continue accepting input
-  });
+    messageInput.value = ''; // Clear input field after sending
 }
 
-sendMessages();
 
-ws.on('message', function incoming(data) {
-  console.log('Received message from server:', data);
-});
 
-ws.on('close', function close() {
-  console.log('Disconnected from WebSocket server');
-});
+/// --------- Old Terminal based! -------------------
+// const WebSocket = require('ws');
+// const readline = require('readline');
+
+// const serverUrl = 'ws://localhost:1200';
+// const ws = new WebSocket(serverUrl);
+
+// ws.on('open', function open() {
+//   console.log('Connected to WebSocket server');
+// });
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// // Function to continuously accept user input and send it to the server
+// function sendMessages() {
+//   rl.question('Enter a message to send to the server: ', function(message) {
+//     ws.send(message);
+//     sendMessages(); // Recursively call sendMessages to continue accepting input
+//   });
+// }
+
+// sendMessages();
+
+// ws.on('message', function incoming(data) {
+//   console.log('Received message from server:', data);
+// });
+
+// ws.on('close', function close() {
+//   console.log('Disconnected from WebSocket server');
+// });
--- server_ws.js
+++ server_ws.js
@@ -7,14 +7,24 @@ const clients = []; // Array to store connected WebSocket instances
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
   clients.push(ws);
+  
+  const clientIndex = clients.indexOf(ws);
+  console.log(`Client ${clientIndex+1} connected`);
+  
 
   ws.on('message', function incoming(data) {
-    console.log('received: %s', data);
+    console.log('Received from %d client: %s', clientIndex+1, data);
     // Broadcast the received message to all connected clients
     // clients.forEach(client => {
     //   client.send(data);
     // });
     ws.send(data.toString());
+  });
+
+  ws.on('close', function() {
+    const clientIndex = clients.indexOf(ws);
+    console.log(`Client ${clientIndex+1} disconnected`);
+    clients.splice(clientIndex, 1); // Remove disconnected client from array
   });
 
   ws.send('Connected to the server');
